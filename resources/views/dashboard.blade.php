3a7a027d87eb8547d45e74ebfafed2c35d13b8cc
Ubuntu <ubuntu@ip-172-31-30-243.ec2.internal>
2023-03-29 06:11:43 +0000

    0329

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,36 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800  leading-tight">
+            {{ __('co-animal TOP') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white  overflow-hidden shadow-sm sm:rounded-lg">
+                <img src="{{ asset('images/lp_image02.png') }}" alt="">
+            </div>
+        </div>
+                
+                
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">      
+            <div class="text-center">
+                <p  class="font-semibold text-gray-600  focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">新着順</p>
+                <!--新着の投稿をアップする-->
+                <!--フロントに全部出すデータ-->
+                <a href="content"  class="font-semibold text-gray-600 hover:text-gray-900 focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">{{ __('もっと見る') }}</a>
+                </div>
+                <div class="text-center">
+                <!--<p>おすすめアカウント</p>-->
+                <!--おすすめアカウントを1つアップする-->
+                    <a href="partner" class="hidden">
+                        {{ __('もっと見る') }}
+                    </a>
+            </div>
+        </div>
+        <div>
+            <a href="/service"><img src="{{ asset('images/banner01.png') }}" alt="" class="mx-auto"></a>
+        </div>
+    </div>
+    
+</x-app-layout>
---
+++ routes/web.php
@@ -0,0 +1,65 @@
+<?php
+
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TweetController;
+use App\Http\Controllers\PartnerController;
+use App\Http\Controllers\ContentController;
+use App\Http\Controllers\FavoriteController;
+use App\Http\Controllers\CommentController;
+use App\Http\Controllers\Content;
+
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::middleware('auth')->group(function () {
+    // いいねの機能
+    Route::post('content/{content}/favorites', [FavoriteController::class, 'store'])->name('favorites');
+    Route::post('content/{content}/unfavorites', [FavoriteController::class, 'destroy'])->name('unfavorites');
+    Route::get('/content/mypage', [ContentController::class, 'mydata'])->name('content.mypage');
+    Route::resource('content', ContentController::class);
+    Route::post('content/{content_id}/comments', [CommentController::class, 'store'])->name('comment.store');
+});
+
+Route::resource('tweet', TweetController::class);
+Route::resource('partner', PartnerController::class);
+Route::resource('content', ContentController::class);
+
+
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/service', function () {
+    return view('service');
+});
+
+Route::get('/contact', function () {
+    return view('contact');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+
+
+require __DIR__.'/auth.php';
